dd774b6fc15694168002d523a0b3f7ccaa7c21d8
James <88713216+TJPGuyn@users.noreply.github.com>
2022-03-11 19:55:20 +0000

    Added a decription of the rules

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+#Go to wikipedia
+#Scan/read the page
+#click on the nth link (<a>)
+#continue scanning pages and going to the nth link until you:
+#a. arrive at a page with less than n links
+#b. return to a page that you have already visited.
+#c. the nth link is an empty link
+#d. you land on a non-wikipedia page
+
+
+import urllib
+import bs4
+
+
 n = input("Choose an iteger 'n': ")
+
+def play(n):
+  
